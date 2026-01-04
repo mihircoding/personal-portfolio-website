c99152a7db16fca27f3f09f2ea5a66a5be44bd2c
mihircoding <mihir.konda@gmail.com>
2026-01-04 15:30:52 -0500

    updated

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import { Menu, X } from "lucide-react";
 import { useEffect, useState } from "react";
 
 const navLinks = [
-  { href: "#about", label: "About" },
+  { href: "#about", label: "Abouts" },
   { href: "#projects", label: "Projects" },
   { href: "#experience", label: "Experience" },
 ];
